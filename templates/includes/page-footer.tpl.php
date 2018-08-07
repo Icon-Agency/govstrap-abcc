16b761c80ac3e4197d89c78412ee936a6f56f85f
Jason Guo <jason@xing.com.au>
2018-08-08 09:34:29 +1000

    add page footer and content area to page template

---
+++ templates/includes/page-footer.tpl.php
@@ -0,0 +1,83 @@
+<footer class="footer bg-footer text-white">
+    <div class="container py-5">
+        <div class="row">
+            <div class="col-lg-8">
+                <h3 class="text-white mb-4">Need assistance?</h3>
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="item phone">
+                            <img src="/<?php print path_to_theme(); ?>/images/icon-call.svg">
+                            <span class="text-uppercase">
+                                                Call ABCC hotline
+                                                <a href="tel:1800003338">
+                                                    <strong class="text-green">
+                                                        1800 003 338
+                                                    </strong>
+                                                </a>
+                                            </span>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <a href="#" class="item">
+                            <img src="/<?php print path_to_theme(); ?>/images/icon-contact-form.svg">
+                            <span class="text-uppercase">
+                                                Use our
+                                                <strong class="text-green">
+                                                    contact form
+                                                    <i class="fal fa-arrow-circle-right"></i>
+                                                </strong>
+                                            </span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <h3 class="text-white mb-4 hide-xs-down">Stay up to date</h3>
+                <a href="#" class="item">
+                    <img src="/<?php print path_to_theme(); ?>/images/icon-email.svg">
+                    <span class="text-uppercase">
+                                        E-alerts &amp; industry updates
+                                        <strong class="text-blue">
+                                            Subscribe
+                                            <i class="fal fa-arrow-circle-right"></i>
+                                        </strong>
+                                    </span>
+                </a>
+            </div>
+        </div>
+    </div>
+    <div class="bg-footer-alt py-4">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8">
+                    <ul class="font-family3">
+                        <li>
+                            <a href="#">Copyright</a>
+                        </li>
+                        <li>
+                            <a href="#">Help</a>
+                        </li>
+                        <li>
+                            <a href="#">Site map</a>
+                        </li>
+                        <li>
+                            <a href="#">Terms of use</a>
+                        </li>
+                        <li>
+                            <a href="#">Contact ABCC</a>
+                        </li>
+                        <li>
+                            <a href="#">Site by Icon</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-lg-4">
+                    <a href="#" class="btn alt btn-outline-primary">
+                        Explore the website
+                        <i class="fal fa-bars ml-4"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
