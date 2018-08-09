1c2e34f55fb9c54ec340d8fa8c35cd92f018e8c9
Jason Guo <jason@xing.com.au>
2018-08-09 22:31:47 +1000

    added image tiles

--- templates/views/views-view-unformatted--abcc-homepage-content-feed--block.tpl.php
+++ templates/views/views-view-unformatted--abcc-homepage-content-feed--block.tpl.php
@@ -8,10 +8,24 @@
  */
 ?>
 <?php if (!empty($title)): ?>
-  <h3><?php print $title; ?></h3>
+    <h3><?php print $title; ?></h3>
 <?php endif; ?>
 <div class="row">
-<?php foreach ($rows as $id => $row): ?>
+    <div class="col-lg-8">
+        <a href="#" class="card img-card h-100 fade-in-up animated">
+            <img src="/<?php print path_to_theme(); ?>/images/sp-need-to-know.jpg"
+                 alt=" Get the ABCC app promo image">
+            <span class="sr-only">Need to know your rights on site. Get the app</span>
+        </a>
+    </div>
+  <?php foreach ($rows as $id => $row): ?>
     <?php print $row; ?>
-<?php endforeach; ?>
+  <?php endforeach; ?>
+    <div class="col-lg-8">
+        <a href="#" class="card img-card h-100 fade-in-up animated">
+            <img src="/<?php print path_to_theme(); ?>/images/sp-subscribe.jpg"
+                 alt="Subscribe for e-alerts promo image">
+            <span class="sr-only">Subscribe for e-alerts &amp; industry updates - Subscribe now</span>
+        </a>
+    </div>
 </div>
