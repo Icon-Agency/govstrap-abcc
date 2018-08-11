6879f46f77e7bf4cfa4c2b48f868694f5cb08eab
Jason Guo <jason@xing.com.au>
2018-08-11 16:29:16 +1000

    load industry update chapter links from book menu

--- panels-layouts/homepage/panels-homepage.tpl.php
+++ panels-layouts/homepage/panels-homepage.tpl.php
@@ -54,7 +54,35 @@
                     <div class="col-xl-8 offset-xl-4 col-lg-9 offset-lg-3 py-5">
                         <div class="text">
                             <div class="accent color-green">
-                              <?php print $content['row-3']; ?>
+                              <?php
+                                $book_news_and_media_bid = variable_get('book_news_and_media_bid', 4);
+                                $book_news_and_media_menu_tree = menu_tree_all_data('book-toc-'.$book_news_and_media_bid)
+                                $book_latest_industry_update = $book_news_and_media_menu_tree["50000 News and Media 491"]["below"]["50028 Industry Update 622"]["below"]["49999 Latest Industry Update 1870"]["below"];
+                                if ($book_news_and_media_menu_tree) {
+                                  $book_latest_industry_update = array_shift(array_slice($book_latest_industry_update, 0, 1));
+                                  $latest_industry_updates['links'] = $book_latest_industry_update['below'];
+                                  $latest_industry_update['title'] = trim(str_replace('Industry Update', '', $book_latest_industry_update["link"]["link_title"]));
+                                }
+                              ?>
+                                <?php if ($book_latest_industry_update): ?>
+                                    <span class="font-family3 text-uppercase small"><strong class="text-green">Issue </strong> — <?php print $latest_industry_update['title']; ?></span>
+                                    <h2 class="h1 text-green mb-4">Industry update <i class="fal fa-arrow-circle-right fa-xs ml-1"></i></h2>
+                                    <ul class="list-unstyled fixed-number-list font-family3 color-white large">
+                                        <?php
+                                          $chapter_index = 0;
+                                          $chapter_prefix = '0';
+                                          if (count($book_latest_industry_update['below']) >= 10) {
+                                            $chapter_prefix = '';
+                                          }
+                                          foreach ($book_latest_industry_update['below'] as $book_latest_industry_update_chapter): ?>
+                                          <?php $chapter_index++; ?>
+                                            <li class="mb-1"><span class="icon text-green"><?php print $chapter_prefix.$chapter_index; ?>.</span>
+                                                <?php print l($book_latest_industry_update_chapter["link"]["title"], $book_latest_industry_update_chapter["link"]["link_path"]); ?>
+                                            </li>
+                                          <?php endforeach; ?>
+                                    </ul>
+                                <?php endif; ?>
+                                <?php print print $content['row-3']; ?>
                             </div>
                         </div>
                     </div>
