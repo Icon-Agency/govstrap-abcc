9ecede8b788751e139bf1c3ee79b720cfc1dc8ff
Jason Guo <jason@xing.com.au>
2018-09-25 16:07:29 +1000

    homepage campaign

--- templates/views/views-view-unformatted--abcc-homepage-content-feed--block.tpl.php
+++ templates/views/views-view-unformatted--abcc-homepage-content-feed--block.tpl.php
@@ -12,20 +12,20 @@
 <?php endif; ?>
 <div class="row">
     <div class="col-lg-8">
-        <a href="/app" class="card img-card h-100 fade-in-up animated">
-            <img src="/<?php print path_to_theme(); ?>/images/sp-need-to-know.png"
-                 alt=" Get the ABCC app promo image">
-            <span class="sr-only">Need to know your rights on site. Get the app</span>
+        <a href="https://www.eventbrite.com/e/security-of-payment-information-session-for-construction-contractors-tickets-50381811354" target="_blank" class="no-icon card img-card h-100 fade-in-up animated">
+            <img src="/<?php print path_to_theme(); ?>/images/sp-sub_contractor.jpg"
+                 alt="sub-contractor campaign promo image">
+            <span class="sr-only">Subcontractors deserve to be paid on time. Register for the free NSW info session</span>
         </a>
     </div>
   <?php foreach ($rows as $id => $row): ?>
     <?php print $row; ?>
   <?php endforeach; ?>
     <div class="col-lg-8">
-        <a href="#" class="card img-card h-100 fade-in-up animated">
-            <img src="/<?php print path_to_theme(); ?>/images/sp-subscribe.png"
-                 alt="Subscribe for e-alerts promo image">
-            <span class="sr-only">Subscribe for e-alerts &amp; industry updates - Subscribe now</span>
+        <a href="/app" class="card img-card h-100 fade-in-up animated">
+            <img src="/<?php print path_to_theme(); ?>/images/sp-need-to-know.png"
+                 alt=" Get the ABCC app promo image">
+            <span class="sr-only">Need to know your rights on site. Get the app</span>
         </a>
     </div>
 </div>
