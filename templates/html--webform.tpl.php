2265073e0e520354ec141855f1168ae42864eee6
Jason Guo <jason@xing.com.au>
2018-09-27 15:29:02 +1000

    copied the live site Google Tag Manager script to the end of <body>

--- templates/html--webform.tpl.php
+++ templates/html--webform.tpl.php
@@ -93,5 +93,13 @@
 
 <?php print $page_bottom ?>
 <?php print $scripts ?>
+<!-- Google Tag Manager -->
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-KCP8H5" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-KCP8H5');</script>
+<!-- End Google Tag Manager -->
 </body>
 </html>
