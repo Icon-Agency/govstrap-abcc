8ecae45e82977268c6052e58ca77519e334fa6bc
Steve Gong <stevegong2016@gmail.com>
2018-10-02 16:29:55 +1000

    bh-469: adding first and last featured content feed from configurable block instead of hard code.

--- templates/views/views-view-unformatted--abcc-homepage-content-feed--block.tpl.php
+++ templates/views/views-view-unformatted--abcc-homepage-content-feed--block.tpl.php
@@ -11,21 +11,19 @@
     <h3><?php print $title; ?></h3>
 <?php endif; ?>
 <div class="row">
-    <div class="col-lg-8">
-        <a href="https://www.eventbrite.com/e/security-of-payment-information-session-for-construction-contractors-tickets-50381811354" target="_blank" class="no-icon card img-card h-100 fade-in-up animated">
-            <img src="/<?php print path_to_theme(); ?>/images/sp-sub_contractor.jpg"
-                 alt="sub-contractor campaign promo image">
-            <span class="sr-only">Subcontractors deserve to be paid on time. Register for the free NSW info session</span>
-        </a>
-    </div>
+  <?php $block = module_invoke('bean', 'block_view', 'first-featured-content-feed'); ?>
+  <?php if ($block): ?>
+      <div class="col-lg-8">
+        <?php print render($block['content']); ?>
+      </div>
+  <?php endif; ?>
   <?php foreach ($rows as $id => $row): ?>
     <?php print $row; ?>
   <?php endforeach; ?>
-    <div class="col-lg-8">
-        <a href="/app" class="card img-card h-100 fade-in-up animated">
-            <img src="/<?php print path_to_theme(); ?>/images/sp-need-to-know.png"
-                 alt=" Get the ABCC app promo image">
-            <span class="sr-only">Need to know your rights on site. Get the app</span>
-        </a>
-    </div>
+  <?php $block = module_invoke('bean', 'block_view', 'last-featured-content-feed'); ?>
+  <?php if ($block): ?>
+      <div class="col-lg-8">
+        <?php print render($block['content']); ?>
+      </div>
+  <?php endif; ?>
 </div>
